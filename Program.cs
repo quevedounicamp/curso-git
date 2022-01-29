5420e0f084684239d61eb189e476b16d74382212
quevedounicamp <quevedo.unicamp@gmail.com>
2022-01-29 17:08:02 -0300

    Acrescentada mensagem desejando bom dia

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@ namespace Prog_teste_git {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Olá mundo!");
-            Console.WriteLine("Bom dia!");
             Console.WriteLine("Como você está?");
+            Console.WriteLine("Tenha um bom dia!");
 
             Console.ReadLine();
         }

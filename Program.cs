bcc7e3136d4ea86db3746a8b45f263ff7c5aa2a7
quevedounicamp <quevedo.unicamp@gmail.com>
2022-01-29 16:32:29 -0300

    Projeto iniciado mostrando mensagem basica

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Prog_teste_git {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Olá mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
